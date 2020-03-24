206dff700a41b8bdd9dc5eb2c3a1624462432f0d
Fabrício <fabriciomicael@hotmail.com>
2020-03-24 18:52:47 +0000

    Dinamic view loader v1

--- application/controllers/Posts.php
+++ application/controllers/Posts.php
@@ -1,12 +1,21 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Posts extends CI_Controller {
+class Posts extends CI_Controller
+{
+	//Dinamic header, view and footer loader
+	public function load_view($view,$data=NULL)
+	{
+		$this->load->view('templates/header');
+		$this->load->view($view['name'],$data);
+		$this->load->view('templates/footer');
+	}
 
 	public function index()
 	{
-		$this->load->view('templates/header');
-		$this->load->view('Show');
-		$this->load->view('templates/footer');
+		//Declaring wich view to load
+		$view['name'] = 'Show';
+
+		$this->load_view($view);
 	}
 }
