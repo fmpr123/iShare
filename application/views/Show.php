4143b58913a00d3536ddf10768a07846b9e3cb80
Fabrício <fabriciomicael@hotmail.com>
2020-04-07 16:42:15 +0100

    main page start

--- application/views/Show.php
+++ application/views/Show.php
@@ -1,4 +1,11 @@
-<?php print_r($this->session->userdata());?>
+<?php print_r($this->session->userdata()); ?>
 <br>
 This is main
 <br>
+<br>
+
+<div>
+    <div>user_image</div>
+    <div>content</div>
+    <div>post_image</div>
+</div>
