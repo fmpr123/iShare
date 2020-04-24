c90bf8bc0f6351664f76a5542c978e22dc1f80e9
Fabrício <fabriciomicael@hotmail.com>
2020-04-24 17:45:46 +0100

    post layout

--- application/views/Show.php
+++ application/views/Show.php
@@ -1,11 +1,38 @@
-<?php print_r($this->session->userdata()); ?>
 <br>
 This is main
 <br>
 <br>
 
-<div>
-    <div>user_image</div>
-    <div>content</div>
-    <div>post_image</div>
+<div class="d-flex justify-content-center">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-2 userimg_div">
+                <p class="username">@Fabrício</p>
+                <img class="user_image" src="images/user_placeholder.png">
+            </div>
+            <div class="col-lg-3 content">
+                <div class="row">
+                    <p> Lorem ipsum <small class="text-muted"> 2020-03-09 12:35:10</small></p>
+                    <div class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"></a>
+                        <div class="dropdown-menu" style="">
+                            <a class="dropdown-item" href="#">Editar</a>
+                            <a class="dropdown-item" href="#">Privado</a>
+                            <a class="dropdown-item" href="#">Reportar</a>
+                        </div>
+                    </div>
+                </div>
+                <p>Conteúdo</p>
+                <a href="">Link</a>
+                <div class="row">
+                    <p class="tagStyle">Tag</p>
+                    <p class="tagStyle">Tag</p>
+                    <p class="tagStyle">Tag</p>
+                </div>
+            </div>
+            <div class="col-lg-2 postimg_div">
+                <img class="post_image" src="images/user_placeholder.png">
+            </div>
+        </div>
+    </div>
 </div>
